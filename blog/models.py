88163d71ec5d8051ed9ebadd051923cedc59d7f2
carlos_reyes <carlosarp2000@gmail.com>
2021-11-06 20:53:46 -0500

    correcciones en el modelo

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ import datetime
 # Create your models here.
 
 class Posts(models.Model):
-    title = models.CharField(max_leng=100)
+    title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='blog/images')
     date = models.DateField(datetime.date.today)
